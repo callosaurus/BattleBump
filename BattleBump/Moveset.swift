48679eb3390d1db44c21c07b425eff9da0504d76
callosaurus <callumdavies421@gmail.com>
2017-06-13 09:21:58 -0700

    Add moveset struct

---
+++ BattleBump/Moveset.swift
@@ -0,0 +1,22 @@
+//
+//  Moveset.swift
+//  BattleBump
+//
+//  Created by Callum Davies on 2017-06-13.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+import Foundation
+
+struct Moveset {
+  var movesetName = "Basic RPS"
+  var numberOfMoves = 3
+  var movesArray = ["Rock", "Paper", "Scissors"]
+  
+  init(name: String, numberOfMoves: Int, movesArray: [String]) {
+    self.movesetName = name
+    self.numberOfMoves = numberOfMoves
+    self.movesArray = movesArray
+  }
+}
+
