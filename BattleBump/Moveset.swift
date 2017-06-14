23007f052b5ee0ba44a8adfcda4294b46b54923b
callosaurus <callumdavies421@gmail.com>
2017-06-13 23:10:22 -0700

    Begin Moveset editing feature

--- BattleBump/Moveset.swift
+++ BattleBump/Moveset.swift
@@ -12,11 +12,15 @@ struct Moveset {
   var movesetName = "Basic RPS"
   var numberOfMoves = 3
   var movesArray = ["Rock", "Paper", "Scissors"]
+  var winningVerbDictionary = ["Rock": ["vsScissors": "crushes"],
+                                       "Paper": ["vsRock": "wraps around"],
+                                       "Scissors": ["vsPaper": "cuts"]]
   
   init(name: String, numberOfMoves: Int, movesArray: [String]) {
     self.movesetName = name
     self.numberOfMoves = numberOfMoves
     self.movesArray = movesArray
   }
+  
 }
 
---
+++ BattleBump/MovesetViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovesetViewController.swift
+//  BattleBump
+//
+//  Created by Callum Davies on 2017-06-13.
+//  Copyright © 2017 Callum Davies & Dave Augerinos. All rights reserved.
+//
+
+import UIKit
+
+class MovesetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
